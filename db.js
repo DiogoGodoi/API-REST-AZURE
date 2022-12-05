c2d06e6e940a0a4ff53736d95119294efe5feac0
Diogo Godoi <65987059+DiogoGodoi@users.noreply.github.com>
2022-12-05 09:23:00 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const SQL = require('mssql')
 const sqlConfig = {
     user: 'Dgodoi',
-    password: 'Aparecido154*',
+    password: '*******',
     server: 'dgodoi.database.windows.net',
     database: 'dbGaleria',
     options: {
